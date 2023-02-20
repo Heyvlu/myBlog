9338f1acf44b688883200e4655aa5e65c5fa36ae
Heyvlu <3059982570@qq.com>
2023-02-21 02:34:14 +0800

    Initialize the project

---
+++ src/pages/Articles/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Article1 from "../../articles/Article1/Article1.mdx";
+
+
+
+function Articles(){
+    return (
+        <div>
+            <Article1 components={{
+                img:({src})=>{
+                    return <img src={"/assets/images/articles/"+src}/>
+                },
+                code:({children})=>{
+                    return <div style={{backgroundColor:"#282c34",color:"#abb2bf"}}>{children}</div>
+                }
+            }}/>
+        </div>
+    )
+}
+
+export default Articles;
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const {EsbuildPlugin} = require('esbuild-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "dist.js",
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+    optimization: {
+        minimizer: [
+            new EsbuildPlugin({
+                target:'es2020'
+            })
+        ]
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname,'src/')
+        },
+        extensions: [".js",".jsx"],
+        mainFiles: ["index"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: [{loader: 'esbuild-loader', options: {target: 'es2020'}}]
+            },
+            {
+                test: /\.scss/i,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        }
+                    },
+                    {loader: "sass-loader"}
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.mdx?$/i,
+                use: [
+                    {
+                        loader: '@mdx-js/loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({template: "./public/index.html"})
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        open: true,
+        hot: true,
+        compress: true,
+        port: 3000
+    }
+}
