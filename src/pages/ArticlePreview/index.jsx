c8972f39dbbeabf7c6cfb730a803b2ff02d4b61d
Heyvlu <3059982570@qq.com>
2023-02-22 06:07:31 +0800

    write article preview module

--- src/pages/ArticlePreview/index.jsx
+++ src/pages/ArticlePreview/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import styles from "./index.scss";
 
 function ArticlePreview(props){
     const {text}=props;
-    console.log(text)
+    const articleTitle=text.match(/\s*#+\s*([\w\W]+?)\s/)[1];
+    const articleContent=text.replaceAll(/[#*!`]/g,'');
     return (
-        <div>
-            <div>
-                {
-                    text.match()
-                }
+        <div className={styles["preview"]}>
+            <div className={styles["title"]}>
+                {articleTitle}
+            </div>
+            <div className={styles["content"]}>
+                {articleContent}
             </div>
         </div>
     )
